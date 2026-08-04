acac20dcbc2012824f6debae1e96fb366f8e154d
Vignesh T <vignesh29.t1997@gmail.com>
2026-08-04 17:23:40 +0530

    assignment_3.ts

---
+++ typescript-training/assignment_datatypes/assignment_3.ts
@@ -0,0 +1,28 @@
+//Assignment -3 (Arrays/Datatypes/Operators)
+/*Create two arrays to store student names ["Suresh","Mahesh","Naresh"] and
+marks [75, 80, 82] Add 10 marks to each students using assignment operators and
+store it into another array, after adding 10 marks identify the average marks of all
+students
+
+Expected Output:
+Updated Marks:
+Suresh: 85
+Mahesh: 90
+Naresh: 92
+Average Marks: 89.0
+*/
+
+let studentName : string[] = ["Suresh","Mahesh","Naresh"];
+let studentMarks : number[] = [75,80,82];
+let total : number=0;
+
+console.log("Updated Marks:");
+for(let i:number=0;i<=2;i++)
+{
+studentMarks[i] += 10;
+console.log(`${studentName[i]}:${studentMarks[i]}`);
+total += studentMarks[i];
+}
+//let averagemarks :number = total/3;
+let averagemarks :number = total/studentMarks.length;
+console.log(`Average Marks:${averagemarks}`);
