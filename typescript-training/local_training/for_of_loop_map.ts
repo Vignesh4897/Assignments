2637fa4a56cf3f550fc29af053a71facec20cd72
Vignesh T <vignesh29.t1997@gmail.com>
2026-08-08 11:01:06 +0530

    for_of_loop_map.ts

--- typescript-training/local_training/for_of_loop_map.ts
+++ typescript-training/local_training/for_of_loop_map.ts
@@ -53,3 +53,15 @@ for(let [rollNum,nameList] of students)
     // or console.log("RollNo  :",rollNum,"NameList:",nameList);
 }
 //console.log("*********************");clear
+/*
+Q: Why does for...of return [key, value] for a Map?
+
+Answer:
+A Map stores its data as key-value pairs. Each item in the Map is an entry containing both a key and its corresponding value. Therefore, during each iteration, for...of returns one entry in the form [key, value]. We can then destructure that array into separate key and value variables.
+
+What you should remember
+Array → Values (10, 20, 30)
+String → Characters ('H', 'e', 'l', 'l', 'o')
+Set → Unique values (10, 20, 30)
+Map → Key-value pairs ([101, "Vignesh"], [102, "Rahul"])
+*/
